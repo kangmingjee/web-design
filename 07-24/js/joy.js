d1c231c9386467bf36efa000f6f0215ecc6bcee5
kangminjee <always_kang@naver.com>
2024-07-24 17:22:29 +0900

    07.24

---
+++ 07-24/js/joy.js
@@ -0,0 +1,38 @@
+$(function(){
+    $(".main > li").mouseover(function(){
+        $(this).find(".sub").stop().slideDown();
+    })
+
+    $(".main > li").mouseout(function(){
+        $(".sub").stop().slideUp();
+    })
+
+    var n = 0;
+    setInterval(function(){
+        if(n == 3){
+            n = 0;
+        }
+        else{
+            n++;
+        }
+        console.log(n)
+        $(".tmove").animate({top : n * (-400) + "px"}, 500);
+
+    },3000)
+
+    $(".contents h2").click(function(){
+        $(".contents h2").removeClass("on");
+        $(this).addClass("on");
+        $(".contents ul").hide();
+        $(this).next().css({display : "flex"})
+    }) //
+
+    $(".p_click").click(function(){
+        $(".pop").show();
+    })//
+    $(".close").click(function(){
+        $(".pop").hide();
+    })//
+
+
+})
---
+++ 07-24/js/jq.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".main > li").mouseover(function(){
+        $(this).find(".sub").stop().slideDown();
+    })
+
+    $(".main > li").mouseout(function(){
+        $(".sub").stop().slideUp();
+    })
+})
